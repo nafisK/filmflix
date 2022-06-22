2ed5aed5fb6bd93b3b72b3ab1b57ba7c00a65663
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-06-22 11:52:17 -0400

    added movie card in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
 // API_KEY = 6185425d
 const API_KEY = "6185425d";
 const API_URL = "http://www.omdbapi.com/?apikey=" + API_KEY;
+
+const movie1 = {
+  Title: "The Avengers",
+  Year: "2012",
+  imdbID: "tt0848228",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+};
 
 const App = () => {
   const searchMovies = async (title) => {
@@ -13,9 +24,47 @@ const App = () => {
   };
 
   useEffect(() => {
-    searchMovies("Avengers");
+    // searchMovies("Avengers");
   }, []);
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <h1>FilmFlix</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for a movie"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="Search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        {/* Movie Container */}
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt="Poster"
+            />
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
