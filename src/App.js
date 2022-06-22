b46bc0521807af8abc01bb0c94eacbbac8b5d0ac
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-06-22 11:13:00 -0400

    added api call and css data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// API_KEY = 6185425d
+const API_KEY = "6185425d";
+const API_URL = "http://www.omdbapi.com/?apikey=" + API_KEY;
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Avengers");
+  }, []);
+  return <div>App</div>;
+};
 
 export default App;
