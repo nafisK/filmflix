eb329ad051118eea178e9f479dd539b77539dc5e
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-06-22 12:24:52 -0400

    added comments to all sections for better understanding

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ const API_KEY = "6185425d";
 const API_URL = "http://www.omdbapi.com/?apikey=" + API_KEY;
 
 const App = () => {
+  // use state to change values within document
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  // gets all movies from the api
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
@@ -17,23 +20,31 @@ const App = () => {
     setMovies(data.Search);
   };
 
+  // gets values when website
   useEffect(() => {
-    searchMovies("Avengers");
+    searchMovies("Movie");
   }, []);
 
   return (
     <div className="app">
       <h1>FilmFlix</h1>
+      // search bar
       <div className="search">
         <input
           type="text"
           placeholder="Search for a movie"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
         />
-        <img src={SearchIcon} alt="Search" onClick={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => {
+            searchMovies(searchTerm);
+          }}
+        />
       </div>
-
+      // movie cards if there are movies
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
